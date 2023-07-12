c9c2c6fd98fe528f227ad73dcac60a3aef5c9290
brustcraft458 <84393872+brustcraft458@users.noreply.github.com>
2023-07-12 20:55:08 +0700

    Main Index

---
+++ main.js
@@ -0,0 +1,8 @@
+import {renderStart, Sprite, camera} from "./src/controler/render.js"
+import {runScript} from "./src/controler/script.js"
+
+// Initialize
+renderStart()
+
+// Script run
+runScript("menu_main")
